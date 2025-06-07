2060484d3d84cf075e15061b2c9ed5a760118c48
Koray <koray.uymaz@agesa.com.tr>
2025-06-07 21:36:26 +0300

    Object separator functionality added

--- src/objects.js
+++ src/objects.js
@@ -5,24 +5,38 @@ export const main = () => {
 	const assignNewPersonButton = document.getElementById("assign-new-person");
 
 	const addNewPropertyButton = document.getElementById("add-new-property");
+	const currentObjectToListButton = document.getElementById(
+		"current-object-to-list"
+	);
+	const listItems = document.getElementById("list");
 
 	const object = {
-		name: "John",
-		age: 30,
-		city: "New York",
+		name: faker.person.fullName(),
+		age: faker.number.int({ min: 18, max: 100 }),
+		city: faker.location.city(),
 	};
 
-	const showObject = () => {
-		objectElement.innerHTML = "";
-		const keys = Object.keys(object);
+	const fakerPersonalInformationList = () => {
+		return Array.from({ length: 10 }, () => ({
+			name: faker.person.fullName(),
+			age: faker.number.int({ min: 18, max: 100 }),
+			city: faker.location.city(),
+		}));
+	};
+
+	const showObject = (objectToShow, element, isList = false) => {
+		if (!isList) {
+			element.innerHTML = "";
+		}
+		const keys = Object.keys(objectToShow);
 		keys.forEach((key) => {
 			const value = document.createElement("div");
-			value.textContent = object[key];
-			objectElement.appendChild(value);
+			value.textContent = objectToShow[key];
+			element.appendChild(value);
 		});
 	};
 
-	showObject();
+	showObject(object, objectElement);
 
 	const updateObject = () => {
 		Object.assign(object, {
@@ -30,17 +44,31 @@ export const main = () => {
 			age: faker.number.int({ min: 18, max: 100 }),
 			city: faker.location.city(),
 		});
-		showObject();
+		showObject(object, objectElement);
 	};
 
 	const addNewProperty = () => {
 		object.bio = faker.person.bio();
-		objectElement.textContent = JSON.stringify(object);
-		showObject();
+		showObject(object, objectElement);
+	};
+
+	const addCurrentObjectToList = () => {
+		const myList = fakerPersonalInformationList();
+		const listWithCurrentObject = [...myList, object];
+		// or myList.push(object); would work too
+		// or myList.reduce((acc, item) => [...acc, item], [object]); would work too
+		// or myList.concat([object]); would work too (but it returns a new array)
+		listWithCurrentObject.forEach((item) => {
+			const newListItem = document.createElement("div");
+			newListItem.classList.add("card");
+			showObject(item, newListItem, true);
+			listItems.appendChild(newListItem);
+		});
 	};
 
 	assignNewPersonButton.addEventListener("click", updateObject);
 	addNewPropertyButton.addEventListener("click", addNewProperty);
+	currentObjectToListButton.addEventListener("click", addCurrentObjectToList);
 };
 
 main();
